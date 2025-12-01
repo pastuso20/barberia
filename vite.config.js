affac3df599ba2cd05a07879ded6b5053e64194e
David Aite <daite870@gmail.com>
2025-12-01 17:44:22 -0500

    Configure Vite base for GitHub Pages and add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Set base path for GitHub Pages project site
+  // If deploying to https://pastuso20.github.io/barberia/, base must be '/barberia/'
+  base: '/barberia/',
   plugins: [react()],
 })
